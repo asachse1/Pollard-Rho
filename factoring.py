f7f4310b4bde533e2b04a3f0f5e26e2e5e5eeb57
asachse1 <42878671+asachse1@users.noreply.github.com>
2020-05-04 02:00:14 -0400

    Header added

---
+++ factoring.py
@@ -0,0 +1,10 @@
+#Name: Adam Sachsel
+#Datae: 05/04/2020
+#Assn: Pollard_Rho_Factor
+#Class: Marron CS441
+#Build Inst: Just install the gmpy2 library
+
+
+import gmpy2
+from gmpy2 import mpz, powmod
+from random import *
